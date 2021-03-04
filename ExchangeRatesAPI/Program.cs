da2784fb263da8baa6599b348fb4ef7da9abb955
Janek Paluch <paluchjanek412@gmail.com>
2021-03-04 14:04:57 +0100

    Wersja z Bazą Danych

--- ExchangeRatesAPI/Program.cs
+++ ExchangeRatesAPI/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 
 
@@ -14,23 +15,168 @@ namespace ExchangeRatesAPI
         const string url = "https://api.exchangeratesapi.io/latest";
         private static readonly HttpClient client = new HttpClient();
 
+        const string connectionString = "Server=LAPTOP-8AROAO4O\\MSSQLSERVERDEVJP;" +
+                                        "Database=ExchangeRatesAPI;" +
+                                        "User Id=sa;" +
+                                        "Password=Janush04072018;";
+
         private static async Task<Format> GetDataFromGit()
         {
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage streamResponse = await client.GetAsync(url);
+            HttpResponseMessage httpResponse = await client.GetAsync(url);
 
-            streamResponse.EnsureSuccessStatusCode();
+            httpResponse.EnsureSuccessStatusCode();
 
-            var result = await streamResponse.Content.ReadAsStreamAsync();
+            var result = await httpResponse.Content.ReadAsStreamAsync();
             var resultList = await JsonSerializer.DeserializeAsync<Format>(result);
 
             return resultList;
+
+        }
+
+        private static void UploadToDB(Format data)
+        {
+            bool exists;
+
+            string createString = "CREATE TABLE Rates( Currency char(3),  rate varchar(10) NOT NULL, PRIMARY KEY (currency))";
+            string checkString = "select case when exists((select* from information_schema.tables where table_name = 'Rates')) then 1 else 0 end";
+            string clearString = "TRUNCATE TABLE Rates";
+            string uploadString;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand checkCommand = new SqlCommand(checkString, connection);
+            SqlCommand uploadCommand;
+            
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            exists = (int)checkCommand.ExecuteScalar() == 1;
+
+            if (!exists)
+            {
+                SqlCommand createCommand = new SqlCommand(createString, connection);
+                createCommand.ExecuteNonQuery();
+            }
+            else
+            {
+                SqlCommand clearCommand = new SqlCommand(clearString, connection);
+                clearCommand.ExecuteNonQuery();
+                
+            }
+               
+            foreach(string key in data.Rates.Keys)
+            {
+                uploadString = $"INSERT INTO rates (Currency, rate) VALUES ('{key}' ,  '{data.Rates[key]}')";
+                uploadCommand = new SqlCommand(uploadString, connection);
+                uploadCommand.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+
+        private static Dictionary<string,double> ReadAllFromDB()
+        {
+            Dictionary<string, double> result = new Dictionary<string, double>();
+            string readString = "SELECT * FROM Rates";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            SqlCommand readCommand = new SqlCommand(readString, connection);
+
+            var response = readCommand.ExecuteReader();
+
+            while (response.Read())
+            {
+                result.Add(response.GetString(0), Convert.ToDouble(response.GetString(1)));
+            }
+
+            connection.Close();
+
+            return result;
+        }
+
+        private static double ReadChosenFromDB(string currency)
+        {
+
+            var dBase = ReadAllFromDB();
+
+            if (dBase.ContainsKey(currency))
+            {
+                return dBase[currency];
+            }
+            else return -1;
+
+            //bool exists;
+            //double result = -1;
+
+            //string readString = $"SELECT * FROM Rates WHERE Currency = '{currency}'";
+            //string checkString = $"select case when exists((select * from rates where Currency = '{currency}')) then 1 else 0 end";
+
+            //SqlConnection connection = new SqlConnection(connectionString);
+
+            //if (connection.State != System.Data.ConnectionState.Open)
+            //    connection.Open();
+
+            //SqlCommand readCommand = new SqlCommand(readString, connection);
+            //SqlCommand checkCommand = new SqlCommand(checkString, connection);
+
+            //exists = (int)checkCommand.ExecuteScalar() == 1;
+
+            //if (exists)
+            //{
+            //    var response = readCommand.ExecuteReader();
+            //    result = Convert.ToDouble(response.GetString(0));   
+            //}
+
+            //connection.Close();
+
+            //return result;
+        }
+
+        private static void UpdateDB(Format data)
+        {
+            string updateString;
+            SqlCommand updateCommand;
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            foreach (string key in data.Rates.Keys)
+            {
+                updateString = $"UPDATE rates set rate = '{data.Rates[key]}' WHERE currency = '{key}'";
+                updateCommand = new SqlCommand(updateString, connection);
+                updateCommand.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+
+        private static void DeleteTab()
+        {
+            string createString = $"drop table rates;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand createCommand = new SqlCommand(createString, connection);
+            if (connection.State != System.Data.ConnectionState.Open)
+                createCommand.Connection.Open();
+
+            createCommand.ExecuteNonQuery();
+
+            connection.Close();
         }
         static async Task Main(string[] args)
         {
             var response = await GetDataFromGit();
+
+            UploadToDB(response);
+            
             int choice;
 
             Console.WriteLine("Welcome");
@@ -39,7 +185,8 @@ namespace ExchangeRatesAPI
             {
                 Console.WriteLine("\nType '1' to see list of exchange rates\n" +
                                   "Or   '2' to convert chosen curency\n" +
-                                  "Or   '3' to exit");
+                                  "Or   '3' to update database\n" +
+                                  "Or   '4' to exit");
 
 
                 if (int.TryParse(Console.ReadLine(), out choice))
@@ -49,14 +196,12 @@ namespace ExchangeRatesAPI
                     {
                         case 1:
 
-                            foreach (string key in response.Rates.Keys)
+                            var result = ReadAllFromDB();
+
+                            foreach (string key in result.Keys)
                             {
-                                Console.WriteLine($"{key} : {response.Rates[key]}");
+                                Console.WriteLine($"{key} : {result[key]}");
                             }
-
-                            Console.WriteLine($"\nBase: {response.Base}");
-
-                            Console.WriteLine($"\nLast updated: {response.Date}");
 
                             break;
 
@@ -64,15 +209,17 @@ namespace ExchangeRatesAPI
 
                             Console.Write("\nName the currency you want to exchange: ");
 
-                            string currency = Console.ReadLine();
+                            string currency = Console.ReadLine().ToUpper();
 
-                            if (response.Rates.ContainsKey(currency))
+                            double value = ReadChosenFromDB(currency);
+
+                            if (value >= 0)
                             {
                                 Console.Write($"Input the amount of {currency} to convert: ");
 
                                 if (double.TryParse(Console.ReadLine(), out double amount))
                                 {
-                                    Console.WriteLine($"\nResult: {amount / response.Rates[currency]} EUR");
+                                    Console.WriteLine($"\nResult: {amount / value} EUR");
                                 }
                                 else
                                 {
@@ -87,6 +234,12 @@ namespace ExchangeRatesAPI
                             break;
 
                         case 3:
+
+                            Console.WriteLine("Updating DataBase...");
+                            UpdateDB(await GetDataFromGit());
+                            break;
+
+                        case 4:
 
                             Console.WriteLine("Exiting...");
 
@@ -104,7 +257,7 @@ namespace ExchangeRatesAPI
                 {
                     Console.WriteLine("Input correct number!");
                 }
-            } while (choice != 3);
+            } while (choice != 4);
         }
-    }
+    }    
 }
